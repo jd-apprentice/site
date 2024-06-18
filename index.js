ff99fc5430338148d561632316119ba88c6693a0
Jonathan <emeraldusk@gmail.com>
2024-06-17 21:43:14 -0300

    refactor: a few changes and comments

--- index.js
+++ index.js
@@ -1,32 +1,44 @@
+// ----------------- Variables ----------------- //
 const aboutMeText = document.querySelector('#about-me');
 const audioPlayerButton = document.querySelector('#audioPlayerButton');
 const audioPlayer = document.querySelector('#audioPlayer');
 
-const defaultStyleButton = 'cursor-pointer mt-4 mx-2 px-4 py-2 bg-red-500 text-white rounded-xl border-2 border-black';
 const defaultText = `Hihi! Jonathan here`;
-const defaultState = 'play 🎵';
-const secondaryState = 'pause 🎵';
-const secondaryStyleButton = 'cursor-pointer mt-4 mx-2 px-4 py-2 bg-blue-500 text-white rounded-xl border-2 border-black';
 const aboutMeUrl = 'https://raw.githubusercontent.com/jd-apprentice/jd-apprentice/main/aboutme';
-
 let isPlaying = false
 
+// ----------------- Functions ----------------- //
+function addIcon(text) {
+    return text + ' 🎵';
+};
+
+function returnStyle(style) {
+    return `cursor-pointer mt-4 mx-2 px-4 py-2 bg-${style}-500 text-white rounded-xl border-2 border-black`;
+};
+
+async function fetchText(url) {
+    const response = await fetch(url);
+    const text = await response.text();
+    return text;
+}
+
+// ----------------- Event Listeners ----------------- //
 audioPlayerButton.addEventListener('click', () => {
     if (!isPlaying) {
         audioPlayer.play();
-        audioPlayerButton.textContent = secondaryState;
-        audioPlayerButton.className = secondaryStyleButton;
+        audioPlayerButton.textContent = addIcon('pause');
+        audioPlayerButton.className = returnStyle('blue');
         isPlaying = !isPlaying;
         return;
     };
 
     audioPlayer.pause();
-    audioPlayerButton.textContent = defaultState;
-    audioPlayerButton.className = defaultStyleButton;
+    audioPlayerButton.textContent = addIcon('play');
+    audioPlayerButton.className = returnStyle('red');
     isPlaying = !isPlaying;
     return;
 });
 
+// ----------------- Main ----------------- //
 audioPlayer.volume = 0.2;
-const fetchText = async (url) => fetch(url).then((response) => response.text());
 fetchText(aboutMeUrl).then((text) => aboutMeText.textContent = text ?? defaultText);
